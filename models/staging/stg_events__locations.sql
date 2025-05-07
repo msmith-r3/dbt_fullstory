eef404669ad0316aaa0994161ba90b5c8e31742b
msmith-r3 <matthew.smith@r-cubed.co.uk>
2025-05-07 10:54:28 +0100

    Update stg_events__locations.sql

--- models/staging/stg_events__locations.sql
+++ models/staging/stg_events__locations.sql
@@ -1,18 +1,18 @@
 select
-    full_session_id,
+    {{ dbt.concat(["device_id", "':'", "session_id"]) }} as full_session_id,
     session_id,
     device_id,
     view_id,
     event_time,
     updated_time,
     processed_time,
-    geo_ip_address,
-    geo_country,
-    geo_region,
-    geo_city,
-    geo_lat_long,
+    source_properties.geo_ip_address,
+    source_properties.geo_country,
+    source_properties.geo_region,
+    source_properties.geo_city,
+    source_properties.geo_lat_long,
     row_number() over (
-        partition by full_session_id
+        partition by {{ dbt.concat(["device_id", "':'", "session_id"]) }}
         order by
             event_time desc,
             updated_time desc,
