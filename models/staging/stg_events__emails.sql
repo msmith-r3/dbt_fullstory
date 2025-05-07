56cacbf689877e0c62f9a3c7a5989791649b9d1b
msmith-r3 <matthew.smith@r-cubed.co.uk>
2025-05-07 10:55:35 +0100

    Update stg_events__emails.sql

--- models/staging/stg_events__emails.sql
+++ models/staging/stg_events__emails.sql
@@ -1,18 +1,18 @@
 select
-    full_session_id,
+    {{ dbt.concat(["device_id", "':'", "session_id"]) }} as full_session_id,
     session_id,
     device_id,
     view_id,
     event_time,
     updated_time,
     processed_time,
-    user_email,
+    event_properties.user_email,
     row_number() over (
-        partition by full_session_id
+        partition by {{ dbt.concat(["device_id", "':'", "session_id"]) }}
         order by
             event_time desc,
             updated_time desc,
             processed_time desc
     ) as desc_row_num
 from {{ ref("events") }}
-where user_email is not null
+where event_properties.user_email is not null
