da79aeaaa86c39028cff9b6d12e825c8c970a451
msmith-r3 <matthew.smith@r-cubed.co.uk>
2025-05-12 08:34:18 +0100

    Update stg_events__devices.sql

--- models/staging/stg_events__devices.sql
+++ models/staging/stg_events__devices.sql
@@ -6,11 +6,11 @@ select
     event_time,
     updated_time,
     processed_time,
-    source_properties.device_user_agent,
-    source_properties.device_type,
-    source_properties.device_operating_system,
-    source_properties.device_browser,
-    source_properties.device_browser_version,
+    source_properties.device_user_agent::varchar(65535),
+    source_properties.device_type::varchar(65535),
+    source_properties.device_operating_system::varchar(65535),
+    source_properties.device_browser::varchar(65535),
+    source_properties.device_browser_version::varchar(65535),
     row_number() over (
         partition by {{ dbt.concat(["device_id", "':'", "session_id"]) }}
         order by
